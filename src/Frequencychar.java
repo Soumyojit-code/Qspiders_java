501b395e00669c966f4bd992ce4dc9d221b36474
Soumyojit Banerjee <144514288+Codding-c@users.noreply.github.com>
2026-07-02 11:25:44 +0530

    count Frequency

---
+++ src/Frequencychar.java
@@ -0,0 +1,26 @@
+public class Frequencychar
+{
+    public static void main(String[] args)
+    {
+        String str = "Soumyojit";
+        countFrequency(str);
+    }
+    public static void countFrequency(String str)
+    {
+        int[] fre = new int[256];
+        for (int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            fre[ch]++;
+        }
+        for (int i = 0; i < fre.length; i++)
+        {
+            if (fre[i] > 0)
+            {
+                System.out.println("Count of '" + (char) i + "' is: " + fre[i]);
+            }
+
+
+        }
+    }
+    }
