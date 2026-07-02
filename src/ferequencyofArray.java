94247f257729202bcbe6930f9145206a917221f7
Soumyojit Banerjee <144514288+Codding-c@users.noreply.github.com>
2026-07-02 11:41:58 +0530

    count Freadfquency

--- src/ferequencyofArray.java
+++ src/ferequencyofArray.java
@@ -1,24 +1,19 @@
-public class ferequencyofArray
-{
-    public static void main(String[] args)
-    {
-        int arr[]={10,20,30,20,40,20,10};
+public class ferequencyofArray {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 20, 40, 20, 10};
         array(arr);
     }
-    public static void array(int []arr)
-    {
-        for(int i=0;i<arr.length;i++)
-        {
-            int c=0;
-            for(int j=0;j<arr.length;j++)
-            {
-                if(arr[i]==arr[j])
-                {
+
+    public static void array(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int c = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
                     c++;
                 }
 
             }
-            System.out.println("Count of '" + arr[i] + "' is: " +c);
+            System.out.println("Count of '" + arr[i] + "' is: " + c);
         }
     }
 }
