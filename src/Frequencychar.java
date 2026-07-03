d1332463b2436c4a73f88ad707318c47556cf442
Soumyojit Banerjee <144514288+Codding-c@users.noreply.github.com>
2026-07-03 21:49:28 +0530

    count Freadfquency

--- src/Frequencychar.java
+++ src/Frequencychar.java
@@ -7,7 +7,7 @@ public class Frequencychar
     }
     public static void countFrequency(String str)
     {
-        int[] fre = new int[256];
+        int[] fre = new int[26];
         for (int i = 0; i < str.length(); i++)
         {
             char ch = str.charAt(i);
--- src/Qspider_java/Increasing.java
+++ src/Qspider_java/Increasing.java
@@ -2,23 +2,19 @@
 //        e.g.123489->true(1<2<3<4<8<9)
 package Qspider_java;
 
-public class Increasing
-{
-    public static void main(String[] args)
-    {
-        int num=12342;
-        System.out.println(CountD( num));
-
+public class Increasing {
+    public static void main(String[] args) {
+        int num = 12342;
+        System.out.println(CountD(num));
     }
-    public static boolean CountD(int num)
-    {
-        int ld=10;
-        while(num>0)
-        {
-            int rem=num%10;
-            if(rem>=ld) return false;
-            ld=rem;
-            num=num/10;
+
+    public static boolean CountD(int num) {
+        int ld = 10;
+        while (num > 0) {
+            int rem = num % 10;
+            if (rem >= ld) return false;
+            ld = rem;
+            num = num / 10;
         }
         return true;
     }
