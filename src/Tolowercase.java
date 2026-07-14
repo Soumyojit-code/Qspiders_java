1635ef623e721752f38b62f8b64fe1958b512bc0
Soumyojit Banerjee <144514288+Codding-c@users.noreply.github.com>
2026-07-14 22:25:51 +0530

    LeetCode 709. To Lower Case

---
+++ src/Tolowercase.java
@@ -0,0 +1,15 @@
+//LeetCode 709. To Lower Case
+
+
+public class Tolowercase {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(convert(s));
+    }
+
+    static String convert(String s) {
+
+        return s.toLowerCase();
+
+    }
+}
